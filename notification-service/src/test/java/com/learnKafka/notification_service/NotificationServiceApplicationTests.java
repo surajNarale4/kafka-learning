23494d7372f4c68928caa14a9b3675601cee59a3
surajNarale4 <rajnarale415@gmail.com>
2026-06-07 00:47:24 +0530

    This Project is for learning kafka

---
+++ notification-service/src/test/java/com/learnKafka/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnKafka.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
